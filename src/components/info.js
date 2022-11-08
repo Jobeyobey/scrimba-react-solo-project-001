1d1c422195798fbaeff306fe82782b69a79b2c70
Jobeyobey <117482381+Jobeyobey@users.noreply.github.com>
2022-11-08 01:37:18 +0000

    styled and finalised
    Styled and added finishing touches e.g. links, any missing font awesome, adjusting fonts

--- src/components/info.js
+++ src/components/info.js
@@ -1,16 +1,28 @@
 import React from "react"
 import joe from "../images/joe-hollands.jpg"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+
+function noSite(event) {
+    window.alert("I don't actually have a website");
+    event.preventDefault();
+}
 
 export default function Info() {
     return (
-        <div>
+        <div className="info-container">
             <img src={joe} alt="Picture of Joe Hollands with a coffee" id="profile-image"/>
             <h2>Joe Hollands</h2>
             <h3>Front-End Developer</h3>
-            <a href="https://github.com/Jobeyobey" target="_blank" rel="noreferrer">My Github</a>
-            <div className="buttons">
-                <button type="button">Email</button>
-                <button type="button">GitHub</button>
+            <a href="" target="#" onClick={noSite}>jphollands.co.uk</a>
+            <div className="button-container">
+                <a href="mailto:hollands.joe@gmail.com" target="_blank" rel="noreferrer">
+                    <button type="button" className="email"><FontAwesomeIcon icon={faEnvelope} /> Email</button>
+                </a>
+                <a href="https://github.com/Jobeyobey" target="_blank" rel="noreferrer">
+                    <button type="button" className="github"><FontAwesomeIcon icon={faGithub} /> GitHub</button>
+                </a>
             </div>
         </div>
     )
